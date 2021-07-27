834729d40de242f9b8b237983cb134d205ab11f8
Tijana Maksimovic <tijana.maksimovic@ebcont.com>
2021-07-27 13:25:46 +0200

    add app context and react router

---
+++ src/routes/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+export const AppRoutes = () => {
+  return <Router>{/*<Route path="*" />*/}</Router>;
+};
